420f2c985ecb59f178ae918419f25a917a694f82
Румен Драганов <r.draganov@respect93.com>
2015-09-07 22:19:18 +0300

    Модул 09 Видео 05 Добавяне библиотеки на Excel

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -1,14 +1,50 @@
 ﻿using System;
+using InteropExcel = Microsoft.Office.Interop.Excel;
 
 namespace Excel
 {
 	
 	public class IOWrite
 	{
-		
+		private DataStruct _data;
+		private InteropExcel.Application excel;
 		public IOWrite (DataStruct data)
 		{
 		}
+
+		public bool exportTable()
+		{
+			try{
+
+				return true;
+			}catch{
+			}
+			return false;
+		}
+
+		public void addRow(DataRow _row)
+		{
+			try {
+
+
+			} catch {
+			}
+
+		}
+
+		public void runFile()
+		{
+			try{
+			System.Diagnostics.Process.Start (getPath ());
+			}catch{
+			}
+		}
+
+		public string getPath()
+		{
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "table.xlsx");
+		}
+
 	}
 }
 
